dd66e1afba5d75f1eb827dd8fb27be65e9ec46d6
vareille <junk@ysengrin.com>
2023-04-14 06:54:00 +0200

    v3.12

--- hello_wchar_t.c
+++ hello_wchar_t.c
@@ -1,5 +1,5 @@
 /*_________
- /         \ hello_wchar_t.c v3.11 [Apr 7, 2023] zlib licence
+ /         \ hello_wchar_t.c v3.12 [Apr 14, 2023] zlib licence
  |tiny file| Hello WCHAR_T windows only file created [November 9, 2014]
  | dialogs | Copyright (c) 2014 - 2023 Guillaume Vareille http://ysengrin.com
  \____  ___/ http://tinyfiledialogs.sourceforge.net
@@ -12,7 +12,7 @@
              |                               |
              | this file is for windows only |
              |_______________________________|
-	  
+
 If you like tinyfiledialogs, please upvote my stackoverflow answer
 https://stackoverflow.com/a/47651444
 
