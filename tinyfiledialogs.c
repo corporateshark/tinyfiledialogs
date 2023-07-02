524f8871a94aca0a341dc67ae5b0efb904e09462
Guillaume Vareille <junk@ysengrin.com>
2023-07-02 11:53:50 +0200

    v3.13.3

--- tinyfiledialogs.c
+++ tinyfiledialogs.c
@@ -3,7 +3,7 @@ The code is 100% compatible C C++
 (just comment out << extern "C" >> in the header file) */
 
 /*_________
- /         \ tinyfiledialogs.c v3.13.2 [May 31, 2023] zlib licence
+ /         \ tinyfiledialogs.c v3.13.3 [Jul 2, 2023] zlib licence
  |tiny file| Unique code file created [November 9, 2014]
  | dialogs | Copyright (c) 2014 - 2023 Guillaume Vareille http://ysengrin.com
  \____  ___/ http://tinyfiledialogs.sourceforge.net
@@ -99,7 +99,7 @@ Thanks for contributions, bug corrections & thorough testing to:
 #endif
 #define LOW_MULTIPLE_FILES 32
 
-char tinyfd_version[8] = "3.13.2";
+char tinyfd_version[8] = "3.13.3";
 
 /******************************************************************************************************/
 /**************************************** UTF-8 on Windows ********************************************/
