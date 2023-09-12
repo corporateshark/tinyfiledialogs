f2e299f2200f55028f470d5ed6ff904e72eb5707
bardos <bardos@bardos.local>
2023-09-12 14:12:07 +0200

    v3.14.0

--- dll_cs_lua_R_fortran_pascal/tinyfiledialogs.r
+++ dll_cs_lua_R_fortran_pascal/tinyfiledialogs.r
@@ -1,9 +1,47 @@
+# SPDX-License-Identifier: ZLIB
+# Copyright (c) 2014 - 2023 Guillaume Vareille http://ysengrin.com
+#  _________
+# /         \ tinyfiledialogs v3.14.0 [Sep 12, 2023]
+# |tiny file|
+# | dialogs |
+# \____  ___/ http://tinyfiledialogs.sourceforge.net
+#      \|     git clone http://git.code.sf.net/p/tinyfiledialogs/code tinyfd
+
+# - License -
+# This software is provided 'as-is', without any express or implied
+# warranty.  In no event will the authors be held liable for any damages
+# arising from the use of this software.
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+# 1. The origin of this software must not be misrepresented; you must not
+# claim that you wrote the original software.  If you use this software
+# in a product, an acknowledgment in the product documentation would be
+# appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+# misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+#         ___________________________________________________________
+#        |                                                           |
+#        |     If you like this new R interface please upvote        |
+#        |        my stackoverflow answer on the R post              |
+#        |            https://stackoverflow.com/a/59657117           |
+#        |___________________________________________________________|
+
+
+
+
 # Load the appropriate tinyfd library
 
+# Macintosh
 dyn.load("tinyfiledialogsAppleSilicon.dylib")
 #dyn.load("tinyfiledialogsIntel.dylib")
+
+# Linux on Intel
 #dyn.load("tinyfiledialogsX32.so")
 #dyn.load("tinyfiledialogsX64.so")
+
+# windows on Intel
 #dyn.load("tinyfiledialogs32.dll")
 #dyn.load("tinyfiledialogs64.dll")
 
@@ -107,12 +145,12 @@ tinyfd_colorChooser <- function(aTitle, aDefaultHexRGB) # "#FF0000"
 
 # example R calls to tinyfd functions
 
-#tinyfd_beep()
-#tinyfd_notifyPopup( "a title" , "a message", "warning" )
-#tinyfd_messageBox( "a title" , "a message" , "yesno" , "info" , 0 )
-#tinyfd_inputBox( "a title" , "a message" , "NULL" ) # "NULL" for a password box
-#tinyfd_saveFileDialog( "a title" , "/Users/bardos/Documents/test.txt" , 0 , "" , "")
-#tinyfd_saveFileDialog( "a title" , "/Users/bardos/Documents/test.txt" , 1  , c ("*.txt","*.jpg") , "some files")
-#tinyfd_openFileDialog( "a title" , "/Users/bardos/Documents/" , 1 , c ("*.txt","*.jpg") , "some files" , 0 )
-#tinyfd_selectFolderDialog( "a title" , "/Users/bardos/Devs" )
-#tinyfd_colorChooser( "a title" , "#FF0000" )
+tinyfd_beep()
+tinyfd_notifyPopup( "a title" , "a message", "warning" )
+tinyfd_messageBox( "a title" , "a message" , "yesno" , "info" , 1 )
+tinyfd_inputBox( "a title" , "a message" , "NULL" ) # "NULL" for a password box
+tinyfd_saveFileDialog( "a title" , "/Users/bardos/Documents/test.txt" , 0 , "" , "")
+tinyfd_saveFileDialog( "a title" , "/Users/bardos/Documents/test.txt" , 1  , c ("*.txt","*.jpg") , "some files")
+tinyfd_openFileDialog( "a title" , "/Users/bardos/Documents/" , 1 , c ("*.txt","*.jpg") , "some files" , 0 )
+tinyfd_selectFolderDialog( "a title" , "/Users/bardos/Devs" )
+tinyfd_colorChooser( "a title" , "#FF0000" )
